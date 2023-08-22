b6e7e2342a84d7930ca713c8ae321109b5ef3cd3
Zeenotek <sannisegunrasheed@gmail.com>
2023-08-22 03:36:34 -0700

    withoutgitcodeisntcomplete

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_"
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <stdarg.h>
-#include <stdlib.h>"
+#include <stdlib.h>
 
 typedef int (*ConversionHandler)(va_list);
 
@@ -14,4 +14,4 @@ int _print_string(va_list arguments);
 int _print_char(va_list arguments);
 int handle_format_specifier(char specifier, va_list arguments);
 
-#endif _MAIN_H_
+#endif
