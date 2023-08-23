68bcebba73cdd1b6f9aaa638da1932fec20e25d6
Darlington <obii6776@gmail.com>
2023-08-23 08:52:19 -0700

    done now

--- printf.c
+++ printf.c
@@ -21,13 +21,12 @@ int handle_format_specifier(char specifier, va_list arguments)
 		case '%':
 			write(1, "%", 1);
 			return (1);
+		case 'd':
+		case 'i':
+			handler = _print_int;
+			break;
 		default:
-			write(1, "%", 1);
-			if (specifier != ' ')
-			{
 			write(1, &specifier, 1);
-			return (2);
-			}
 			return (1);
 	}
 	return (handler ? handler(arguments) : 0);
